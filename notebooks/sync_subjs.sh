f26333835ed53941bcba36c0f752b4b205347605
Keyvan Mahjoory <kmahjoory@gmail.com>
2024-02-09 12:59:58 +0100

    Reomve notebooks

--- notebooks/sync_subjs.sh
+++
@@ -1,9 +0,0 @@
-
-subj_id=$1
-
-export dst=~/k1_analyses/prj_neuroflex/neuroflex_analysis/datasets/data/subj_${subj_id}/meg/
-export src=keyvan.mahjoory@hpc-login:/hpc/workspace/2021-0292-NeuroFlex/prj_neuroflex/neuroflex_analysis/datasets/data/subj_${subj_id}/meg/block_*_meg.fif
-
-mkdir -p $dst
-
-rsync -aux $src $dst
